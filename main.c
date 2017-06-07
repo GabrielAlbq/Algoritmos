daca97ac432445668aaa3a65f437c53f8c1cd469
Gabriel Albuquerque de Oliveira <gabriel.oliveira.gt@gmail.com>
2017-06-07 15:49:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-///Funcao que gera um vetor de 'n' posi�oes aleatoriamente.
+void troca (A, B){
+    int t = A; A = B; B = t;
+    }
+
+int contador;
+///Funcao que gera um vetor de 'n' posicoes aleatoriamente.
 int *Aleatorio(int n){
     //int n;
     //n = rand()%1000;
@@ -15,16 +20,19 @@ int *Aleatorio(int n){
     return v;
 }
 void insercao(int n, int v[]){
+    printf("Contador antes %d\n",contador);
     int i,j,x;
     for(j=1;j<n;j++){
         x=v[j];
         for(i=j-1;i>=0 && v[i]>x;i--){
             v[i+1]=v[i];
+            contador++;
         }
             v[i+1] = x;
     }
     for(i=0;i<n;i++)
         printf("%d\n",v[i]);
+    printf("Contador depois %d",contador);
 }
 void selecao(int n, int v[]){
     int i,j,min,x;
@@ -54,20 +62,53 @@ void intercala(int p,int q, int r, int v[]){
     for(i=p;i<r;i++) v[i] =w[i-p];
     free (w);
 }
-void mergesorts(int p, int r, int v[]){
+void mergesort(int p, int r, int v[]){
     int i;
     if(p<r-1){
         int q = (p+r)/2;
-        mergesorts(p,q,v);
-        mergesorts(q,r,v);
+        mergesort(p,q,v);
+        mergesort(q,r,v);
         intercala(p,q,r,v);
     }
     for(i=0;i<r;i++)
         printf("%d\n",v[i]);
 }
+void constroiHeap (int m, int v[]){
+   int k;
+   for (k = 1; k < m; ++k) {
+      // v[1..k] é um heap
+      int f = k+1;
+      while (f > 1 && v[f/2] < v[f]) {  // 5
+         troca (v[f/2], v[f]);          // 6
+         f /= 2;                        // 7
+      }
+   }
+}
+void peneira (int m, int v[]) {
+   int f = 2;
+   while (f <= m) {
+      if (f < m && v[f] < v[f+1])  ++f;
+      // f é o filho mais valioso de f/2
+      if (v[f/2] >= v[f]) break;
+      troca (v[f/2], v[f]);
+      f *= 2;
+   }
+}
+void heapsort (int n, int v[]){
+   int m;
+   constroiHeap (n, v);
+   for (m = n; m >= 2; --m) {
+      troca (v[1], v[m]);
+      peneira (m-1, v);
+   }
+   printf("\n\n");
+   for(m=0;m<n;m++)
+    printf("%d\n",v[m]);
+}
 void menu(){
     int opc,sub;
     long int n;
+    int v = {9,8,7,6,5,4,3,2,1,0};
     printf("1 - Insercao\n");
     printf("2 - Selecao\n");
     printf("3 - Mergesort\n");
@@ -76,10 +117,14 @@ void menu(){
     scanf("%d",&opc);
     if(opc==1)
     {
+        contador = 0;
         system("cls || clear");
+        printf("Contador antes %d\n",contador);
         printf("Digite o numero de posicoes do vetor aleatorio: ");
         scanf("%i",&n);
-        insercao(n,Aleatorio(n));
+        //insercao(n,Aleatorio(n));
+        int v = {9,8,7,6,5,4,3,2,1,0};
+        insercao(10,v);
     }
     else if(opc==2)
     {
@@ -93,19 +138,16 @@ void menu(){
         system("cls || clear");
         printf("Digite o numero de posicoes do vetor aleatorio: ");
         scanf("%i",&n);
-        mergesorts(1,n,Aleatorio(n));
+        mergesort(1,n,Aleatorio(n));
     }
-    /*else if(opc==4)
+    else if(opc==4)
     {
         system("cls || clear");
-        float x;
-        printf("Digite um valor para a potencia");
-        scanf("%d",&sub);
-        system("cls || clear");
-        x=CalcPotRecursivo(sub);
-        printf("2 elevado a %d eh igual a %f",sub,x);
+        printf("Digite o numero de posicoes do vetor aleatorio: ");
+        scanf("%i",&n);
+        heapsort(n,Aleatorio(n));
     }
-    else if(opc==5)
+    /*else if(opc==5)
     {
         system("cls || clear");
         printf("Digite o tamanho do vetor: ");
